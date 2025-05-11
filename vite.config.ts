820e48d663e7738c89b476262c95fe08fb90026c
taevel02 <taevel02@gmail.com>
2025-05-11 17:01:37 +0800

    chore: fix grammar

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/gas-mix-calculater/",
+  base: "/gas-mix-calculator/",
   plugins: [react(), tailwindcss()],
 });
